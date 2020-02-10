b97ee4c7b6546ceedca3b47764150e12519b6249
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 14:38:30 +0100

    Possible transformations

---
+++ sprinkle/main.go
@@ -0,0 +1,17 @@
+package main
+
+const otherWord = "*"
+
+var transform = []string{
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	otherWord + "place",
+	"the" + otherWord,
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets " + otherWord,
+	otherWord + "hq",
+}
+
+func main() {}
