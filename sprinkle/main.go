d146bbd0031988280bb871ce40622e5d89c8e3ae
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-10 14:43:29 +0100

    Replaces otherword with word from stdin using a string taken at random from transform

--- sprinkle/main.go
+++ sprinkle/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
 const otherWord = "*"
 
 var transform = []string{
@@ -14,4 +23,12 @@ var transform = []string{
 	otherWord + "hq",
 }
 
-func main() {}
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		t := transform[rand.Intn(len(transform))]
+		fmt.Println(strings.Replace(t, otherWord, s.Text(), -1))
+	}
+}
