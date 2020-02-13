ce38d237fbfd46922c7d48e51aab0f9a8b9fd26c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 12:56:58 +0100

    The synonyms function for finding related words

--- thesaurus/thesaurus.go
+++ thesaurus/thesaurus.go
@@ -1,5 +1,11 @@
 package thesaurus
 
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
 // BigHuge is the type of the synonyms service used
 type BigHuge struct {
 	APIKey string
@@ -12,4 +18,32 @@ type synonyms struct {
 
 type words struct {
 	Syn []string `json:"syn"`
+}
+
+func (b *BigHuge) Synonyms(term string) ([]string, error) {
+	var syns []string
+
+	response, err := http.Get("http://words.bighugelabs.com/api/2/" +
+		b.APIKey + "/" + term + "/json")
+
+	if err != nil {
+		return syns, errors.New("bighuge: Failed when looking for synonyms for" + term + " " + err.Error())
+	}
+
+	var data synonyms
+	defer response.Body.Close()
+
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
+		return syns, err
+	}
+
+	if data.Noun != nil {
+		syns = append(syns, data.Noun.Syn...)
+	}
+
+	if data.Verb != nil {
+		syns = append(syns, data.Verb.Syn...)
+	}
+
+	return syns, nil
 }
