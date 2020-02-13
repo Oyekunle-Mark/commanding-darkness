f6da385fde1487b513aaadbef16176106dbd41b1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-13 13:23:06 +0100

    The thesauras library

---
+++ synonyms/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Oyekunle-Mark/commanding-darkness/thesaurus"
+)
+
+func main() {
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+
+		if err != nil {
+			log.Fatalln("Failed when looking for synonyms for "+word+" ", err)
+		}
+
+		if len(syns) == 0 {
+			log.Fatalln("Couldn't find any synonyms for " + word)
+		}
+
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
